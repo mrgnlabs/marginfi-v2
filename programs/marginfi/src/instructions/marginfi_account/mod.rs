6fd08dc4ba6cc51bce6903b76ba1b3b33cb250e2
man0s <95379755+losman0s@users.noreply.github.com>
2023-02-01 23:03:06 +0800

    chore(sc): lint

--- programs/marginfi/src/instructions/marginfi_account/mod.rs
+++ programs/marginfi/src/instructions/marginfi_account/mod.rs
@@ -1,13 +1,13 @@
-mod initialize;
-mod deposit;
-mod withdraw;
-mod repay;
 mod borrow;
+mod deposit;
+mod initialize;
 mod liquidate;
+mod repay;
+mod withdraw;
 
-pub use initialize::*;
-pub use deposit::*;
-pub use withdraw::*;
-pub use repay::*;
 pub use borrow::*;
+pub use deposit::*;
+pub use initialize::*;
 pub use liquidate::*;
+pub use repay::*;
+pub use withdraw::*;
