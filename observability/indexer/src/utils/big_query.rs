6ff1b828399abc7933ae32bcb381d672af8cb050
man0s <95379755+losman0s@users.noreply.github.com>
2023-02-13 17:28:35 +0800

    feat(obs): transaction and account indexer

---
+++ observability/indexer/src/utils/big_query.rs
@@ -0,0 +1,36 @@
+use gcp_bigquery_client::model::{table_field_schema::TableFieldSchema, table_schema::TableSchema};
+use lazy_static::lazy_static;
+
+pub const NOT_FOUND_CODE: i64 = 404;
+
+lazy_static! {
+    pub static ref TRANSACTION_SCHEMA: TableSchema = TableSchema::new(vec![
+        TableFieldSchema::string("id"),
+        TableFieldSchema::timestamp("created_at"),
+        TableFieldSchema::timestamp("timestamp"),
+        TableFieldSchema::string("signature"),
+        TableFieldSchema::string("indexing_address"),
+        TableFieldSchema::big_numeric("slot"),
+        TableFieldSchema::string("signer"),
+        TableFieldSchema::bool("success"),
+        TableFieldSchema::string("version"),
+        TableFieldSchema::big_numeric("fee"),
+        TableFieldSchema::string("meta"),
+        TableFieldSchema::string("message"),
+    ]);
+
+    pub static ref ACCOUNT_SCHEMA: TableSchema = TableSchema::new(vec![
+        TableFieldSchema::string("id"),
+        TableFieldSchema::timestamp("created_at"),
+        TableFieldSchema::timestamp("timestamp"),
+        TableFieldSchema::string("owner"),
+        TableFieldSchema::big_numeric("slot"),
+        TableFieldSchema::string("pubkey"),
+        TableFieldSchema::big_numeric("lamports"),
+        TableFieldSchema::bool("executable"),
+        TableFieldSchema::big_numeric("rent_epoch"),
+        TableFieldSchema::string("data"),
+    ]);
+}
+
+pub const DATE_FORMAT_STR: &str = "%Y-%m-%d %H:%M:%S";
---
+++ observability/indexer/src/utils/mod.rs
@@ -0,0 +1,3 @@
+pub mod big_query;
+pub mod protos;
+pub mod transactions_crawler;
