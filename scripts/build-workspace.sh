6b9c67045f35fd08df178ead852c341e880d7be1
man0s <95379755+losman0s@users.noreply.github.com>
2023-12-20 16:34:10 +0800

    ci: wip actions

--- scripts/build-workspace.sh
+++ scripts/build-workspace.sh
@@ -2,20 +2,7 @@
 ROOT=$(git rev-parse --show-toplevel)
 cd $ROOT
 
-cluster=$1
-
-if [ -z "$cluster" ]; then
-    echo "Using no cluster feature"
-    cmd="anchor build"
-
-elif [ "$cluster" = "mainnet" ]; then
-    cmd="anchor build -- --features mainnet-beta"
-elif [ "$cluster" = "devnet" ]; then
-    cmd="anchor build -- --features devnet"
-else
-    echo "Error: Unknown cluster: $cluster"
-    exit 1
-fi
+cmd="anchor build"
 
 echo "Running: $cmd"
 eval "$cmd"
