3684e44a10a6957a852fe04a1b3ca6589c65cfbc
losman0s <95379755+losman0s@users.noreply.github.com>
2024-03-29 19:54:15 +0800

    test: mfi account reg tests

--- programs/marginfi/tests/marginfi_account.rs
+++ programs/marginfi/tests/marginfi_account.rs
@@ -1,5 +1,13 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+use std::str::FromStr;
+
 use anchor_lang::prelude::Clock;
 use anchor_lang::{AccountDeserialize, InstructionData, ToAccountMetas};
+use anyhow::bail;
+use base64::engine::general_purpose::STANDARD;
+use base64::Engine;
 use fixed::types::I80F48;
 use fixed_macro::types::I80F48;
 use fixtures::prelude::*;
@@ -18,8 +26,10 @@ use marginfi::state::{
 use marginfi::{assert_eq_with_tolerance, prelude::*};
 use pretty_assertions::assert_eq;
 
-use solana_program::pubkey::Pubkey;
+use solana_account_decoder::UiAccountData;
+use solana_cli_output::CliAccount;
 use solana_program::{instruction::Instruction, system_program};
+use solana_program::{pubkey, pubkey::Pubkey};
 use solana_program_test::*;
 use solana_sdk::compute_budget::ComputeBudgetInstruction;
 use solana_sdk::timing::SECONDS_PER_YEAR;
@@ -2466,12 +2476,12 @@ async fn marginfi_account_authority_transfer_not_account_owner() -> anyhow::Resu
 
 #[tokio::test]
 async fn account_field_values_reg() -> anyhow::Result<()> {
-    let bank_fixtures_path = "tests/fixtures/account";
+    let account_fixtures_path = "tests/fixtures/marginfi_account";
 
     // Sample 1
 
-    let mut path = PathBuf::from_str(bank_fixtures_path).unwrap();
-    path.push("account_sample_1.json");
+    let mut path = PathBuf::from_str(account_fixtures_path).unwrap();
+    path.push("marginfi_account_sample_1.json");
     let mut file = File::open(&path).unwrap();
     let mut account_info_raw = String::new();
     file.read_to_string(&mut account_info_raw).unwrap();
@@ -2482,11 +2492,122 @@ async fn account_field_values_reg() -> anyhow::Result<()> {
     };
     let account = MarginfiAccount::try_deserialize(&mut STANDARD.decode(data)?.as_slice())?;
 
-    println!("account: {:?}", account);
+    assert_eq!(
+        account.authority,
+        pubkey!("Dq7wypbedtaqQK9QqEFvfrxc4ppfRGXCeTVd7ee7n2jw")
+    );
+
+    let balance_1 = account.lending_account.balances[0];
+    assert!(balance_1.active);
+    assert_eq!(
+        I80F48::from(balance_1.asset_shares),
+        I80F48::from_str("1650216221.466876226897366").unwrap()
+    );
+    assert_eq!(
+        I80F48::from(balance_1.liability_shares),
+        I80F48::from_str("0").unwrap()
+    );
+    assert_eq!(
+        I80F48::from(balance_1.emissions_outstanding),
+        I80F48::from_str("0").unwrap()
+    );
+
+    let balance_2 = account.lending_account.balances[1];
+    assert!(balance_2.active);
+    assert_eq!(
+        I80F48::from(balance_2.asset_shares),
+        I80F48::from_str("0").unwrap()
+    );
+    assert_eq!(
+        I80F48::from(balance_2.liability_shares),
+        I80F48::from_str("3806372611.588862122556122").unwrap()
+    );
+    assert_eq!(
+        I80F48::from(balance_2.emissions_outstanding),
+        I80F48::from_str("0").unwrap()
+    );
+
+    // Sample 2
+
+    let mut path = PathBuf::from_str(account_fixtures_path).unwrap();
+    path.push("marginfi_account_sample_2.json");
+    let mut file = File::open(&path).unwrap();
+    let mut account_info_raw = String::new();
+    file.read_to_string(&mut account_info_raw).unwrap();
+
+    let account: CliAccount = serde_json::from_str(&account_info_raw).unwrap();
+    let UiAccountData::Binary(data, _) = account.keyed_account.account.data else {
+        bail!("Expecting Binary format for fixtures")
+    };
+    let account = MarginfiAccount::try_deserialize(&mut STANDARD.decode(data)?.as_slice())?;
 
     assert_eq!(
         account.authority,
-        pubkey!("J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn")
+        pubkey!("3T1kGHp7CrdeW9Qj1t8NMc2Ks233RyvzVhoaUPWoBEFK")
+    );
+
+    let balance_1 = account.lending_account.balances[0];
+    assert!(balance_1.active);
+    assert_eq!(
+        I80F48::from(balance_1.asset_shares),
+        I80F48::from_str("470.952530958931234").unwrap()
+    );
+    assert_eq!(
+        I80F48::from(balance_1.liability_shares),
+        I80F48::from_str("0").unwrap()
+    );
+    assert_eq!(
+        I80F48::from(balance_1.emissions_outstanding),
+        I80F48::from_str("26891413.388324654086347").unwrap()
+    );
+
+    let balance_2 = account.lending_account.balances[1];
+    assert!(!balance_2.active);
+    assert_eq!(
+        I80F48::from(balance_2.asset_shares),
+        I80F48::from_str("0").unwrap()
+    );
+    assert_eq!(
+        I80F48::from(balance_2.liability_shares),
+        I80F48::from_str("0").unwrap()
+    );
+    assert_eq!(
+        I80F48::from(balance_2.emissions_outstanding),
+        I80F48::from_str("0").unwrap()
+    );
+
+    // Sample 3
+
+    let mut path = PathBuf::from_str(account_fixtures_path).unwrap();
+    path.push("marginfi_account_sample_3.json");
+    let mut file = File::open(&path).unwrap();
+    let mut account_info_raw = String::new();
+    file.read_to_string(&mut account_info_raw).unwrap();
+
+    let account: CliAccount = serde_json::from_str(&account_info_raw).unwrap();
+    let UiAccountData::Binary(data, _) = account.keyed_account.account.data else {
+        bail!("Expecting Binary format for fixtures")
+    };
+    let account = MarginfiAccount::try_deserialize(&mut STANDARD.decode(data)?.as_slice())?;
+
+    assert_eq!(
+        account.authority,
+        pubkey!("7hmfVTuXc7HeX3YQjpiCXGVQuTeXonzjp795jorZukVR")
+    );
+
+    let balance_1 = account.lending_account.balances[0];
+    assert!(!balance_1.active);
+    assert_eq!(
+        I80F48::from(balance_1.asset_shares),
+        I80F48::from_str("0").unwrap()
+    );
+    assert_eq!(
+        I80F48::from(balance_1.liability_shares),
+        I80F48::from_str("0").unwrap()
+    );
+    assert_eq!(
+        I80F48::from(balance_1.emissions_outstanding),
+        I80F48::from_str("0").unwrap()
     );
 
     Ok(())
