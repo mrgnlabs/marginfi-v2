154a938dc97e1d0aead3f82894d403f364e95e62
Jakob <jakob@mrgnresearch.com>
2024-05-15 20:26:08 +0000

    fix: clippy error

--- clients/rust/marginfi-cli/src/macros.rs
+++ clients/rust/marginfi-cli/src/macros.rs
@@ -19,6 +19,7 @@ macro_rules! home_path {
             }
         }
 
+        #[allow(clippy::to_string_trait_impl)]
         impl ToString for $my_struct {
             fn to_string(&self) -> String {
                 self.0.clone()
