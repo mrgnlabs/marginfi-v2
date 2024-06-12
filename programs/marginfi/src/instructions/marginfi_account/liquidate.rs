b7b793e28e3fce40ec31bde1cf2a199b5286bba8
cavemanloverboy <caveycool@gmail.com>
2024-06-12 12:52:44 +0300

    revert unnecessary deser

--- programs/marginfi/src/instructions/marginfi_account/liquidate.rs
+++ programs/marginfi/src/instructions/marginfi_account/liquidate.rs
@@ -429,7 +429,7 @@ pub struct LendingAccountLiquidate<'info> {
         ],
         bump = liab_bank.load()?.insurance_vault_bump
     )]
-    pub bank_insurance_vault: Account<'info, TokenAccount>,
+    pub bank_insurance_vault: AccountInfo<'info>,
 
     pub token_program: Program<'info, Token>,
 }
