a15d1e93ec0408e3f3e69557235a02003e38bb8c
jgur-psyops <jonathan@mrgn.group>
2025-01-20 15:56:58 -0500

    Withdraw happy path test

--- tests/09_withdraw_repay.spec.ts
+++ tests/09_withdraw_repay.spec.ts
@@ -19,12 +19,13 @@ import {
 } from "./rootHooks";
 import {
   assertBNApproximately,
+  assertBNEqual,
   assertI80F48Approx,
   assertI80F48Equal,
   getTokenBalance,
 } from "./utils/genericTests";
 import { assert } from "chai";
-import { borrowIx, depositIx } from "./utils/user-instructions";
+import { borrowIx, depositIx, withdrawIx } from "./utils/user-instructions";
 import { USER_ACCOUNT } from "./utils/mocks";
 import { createMintToInstruction } from "@solana/spl-token";
 import { updatePriceAccount } from "./utils/pyth_mocks";
@@ -80,13 +81,76 @@ describe("Withdraw funds", () => {
 
   it("(user 0) withdraws some token A - happy path", async () => {
     const user = users[0];
+    const userAccKey = user.accounts.get(USER_ACCOUNT);
+    const userAccBefore = await program.account.marginfiAccount.fetch(
+      userAccKey
+    );
+    const balancesBefore = userAccBefore.lendingAccount.balances;
+
     const bank = bankKeypairA.publicKey;
-    const tokenABefore = await getTokenBalance(provider, user.tokenAAccount);
+    const userTokenABefore = await getTokenBalance(
+      provider,
+      user.tokenAAccount
+    );
     const bankBefore = await program.account.bank.fetch(bank);
+    const vaultUsdcBefore = await getTokenBalance(
+      provider,
+      bankBefore.liquidityVault
+    );
 
+    await user.mrgnProgram.provider.sendAndConfirm(
+      new Transaction().add(
+        await withdrawIx(user.mrgnProgram, {
+          marginfiAccount: userAccKey,
+          bank: bank,
+          tokenAccount: user.tokenAAccount,
+          remaining: [
+            bankKeypairA.publicKey,
+            oracles.tokenAOracle.publicKey,
+            bankKeypairUsdc.publicKey,
+            oracles.usdcOracle.publicKey,
+          ],
+          amount: withdrawAmountTokenA_native,
+        })
+      )
+    );
 
-    const user0Account = user.accounts.get(USER_ACCOUNT);
+    const userAccAfter = await program.account.marginfiAccount.fetch(
+      userAccKey
+    );
+    const bankAfter = await program.account.bank.fetch(bank);
+    const vaultUsdcAfter = await getTokenBalance(
+      provider,
+      bankBefore.liquidityVault
+    );
+    const balancesAfter = userAccAfter.lendingAccount.balances;
+    const userTokenAAfter = await getTokenBalance(provider, user.tokenAAccount);
 
+    const withdrawExpected = withdrawAmountTokenA_native.toNumber();
+    if (verbose) {
+      console.log(
+        "User 0 withdrew " +
+          withdrawAmountTokenA +
+          " token A (" +
+          withdrawExpected.toString() +
+          ") native"
+      );
+    }
 
+    // user gains the token A, the liquidity vault loses it....
+    assert.equal(userTokenAAfter, userTokenABefore + withdrawExpected);
+    assert.equal(vaultUsdcAfter, vaultUsdcBefore - withdrawExpected);
+
+    // User loses the shares of Token A...
+    // Since there hasn't been any interest (no Token A borrowed), shares and Token A are 1:1
+    const sharesBefore = wrappedI80F48toBigNumber(
+      balancesBefore[0].assetShares
+    ).toNumber();
+    const sharesAfter = wrappedI80F48toBigNumber(
+      balancesAfter[0].assetShares
+    ).toNumber();
+    assert.equal(sharesAfter, sharesBefore - withdrawExpected);
   });
+
+  // TODO repay, withdraw all, then restore the original balances via deposit/borrow
 });
