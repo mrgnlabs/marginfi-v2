f258b4ec850e88a885f2bc4d12a2eaf37123988f
Jon Gurary <91919816+jgur-psyops@users.noreply.github.com>
2025-02-11 16:05:05 -0500

    Remove ignore fee deploy feature (#298)
    * Removes ignore-fee-deploy feature flag
    
    ---------
    
    Co-authored-by: lastemp <emmanuelochieng797@gmail.com>

--- scripts/build-workspace.sh
+++ scripts/build-workspace.sh
@@ -2,6 +2,6 @@
 ROOT=$(git rev-parse --show-toplevel)
 cd $ROOT
 
-cmd="anchor build --no-idl -- --features ignore-fee-deploy"
+cmd="anchor build --no-idl -- --features"
 echo "Running: $cmd"
 eval "$cmd"
