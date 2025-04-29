4637b99c5fbe131e5aa2637f98b1dd5d4b4343c4
Jon Gurary <91919816+jgur-psyops@users.noreply.github.com>
2025-04-29 02:30:33 -0400

    Type crate 0.1.3 complete (#333)
    * Type crate 0.1.3 (pre publish)

---
+++ type-crate/src/types/bank.rs
@@ -0,0 +1,197 @@
+use bytemuck::{Pod, Zeroable};
+
+use crate::constants::discriminators;
+
+use super::{EmodeSettings, Pubkey, WrappedI80F48};
+
+pub const MAX_ORACLE_KEYS: usize = 5;
+
+#[repr(C)]
+#[derive(Debug, PartialEq, Pod, Zeroable, Copy, Clone)]
+pub struct Bank {
+    pub mint: Pubkey,
+    pub mint_decimals: u8,
+
+    pub group: Pubkey,
+
+    // Note: The padding is here, not after mint_decimals. Pubkey has alignment 1, so those 32
+    // bytes can cross the alignment 8 threshold, but WrappedI80F48 has alignment 8 and cannot
+    pub _pad0: [u8; 7], // 1x u8 + 7 = 8
+
+    pub asset_share_value: WrappedI80F48,
+    pub liability_share_value: WrappedI80F48,
+
+    pub liquidity_vault: Pubkey,
+    pub liquidity_vault_bump: u8,
+    pub liquidity_vault_authority_bump: u8,
+
+    pub insurance_vault: Pubkey,
+    pub insurance_vault_bump: u8,
+    pub insurance_vault_authority_bump: u8,
+
+    pub _pad1: [u8; 4], // 4x u8 + 4 = 8
+
+    /// Fees collected and pending withdraw for the `insurance_vault`
+    pub collected_insurance_fees_outstanding: WrappedI80F48,
+
+    pub fee_vault: Pubkey,
+    pub fee_vault_bump: u8,
+    pub fee_vault_authority_bump: u8,
+
+    pub _pad2: [u8; 6], // 2x u8 + 6 = 8
+
+    /// Fees collected and pending withdraw for the `fee_vault`
+    pub collected_group_fees_outstanding: WrappedI80F48,
+
+    pub total_liability_shares: WrappedI80F48,
+    pub total_asset_shares: WrappedI80F48,
+
+    pub last_update: i64,
+
+    pub config: BankConfig,
+
+    /// Bank Config Flags
+    ///
+    /// - EMISSIONS_FLAG_BORROW_ACTIVE: 1
+    /// - EMISSIONS_FLAG_LENDING_ACTIVE: 2
+    /// - PERMISSIONLESS_BAD_DEBT_SETTLEMENT: 4
+    /// - FREEZE_SETTINGS: 8
+    ///
+    pub flags: u64,
+    /// Emissions APR. Number of emitted tokens (emissions_mint) per 1e(bank.mint_decimal) tokens
+    /// (bank mint) (native amount) per 1 YEAR.
+    pub emissions_rate: u64,
+    pub emissions_remaining: WrappedI80F48,
+    pub emissions_mint: Pubkey,
+
+    /// Fees collected and pending withdraw for the `FeeState.global_fee_wallet`'s cannonical ATA for `mint`
+    pub collected_program_fees_outstanding: WrappedI80F48,
+
+    /// Controls this bank's emode configuration, which enables some banks to treat the assets of
+    /// certain other banks more preferentially as collateral.
+    pub emode: EmodeSettings,
+
+    pub _padding_0: [u8; 8],
+    pub _padding_1: [[u64; 2]; 32], // 16 * 2 * 32 = 1024B
+}
+
+impl Bank {
+    pub const LEN: usize = std::mem::size_of::<Bank>();
+    pub const DISCRIMINATOR: [u8; 8] = discriminators::BANK;
+}
+
+#[repr(C)]
+#[derive(Debug, PartialEq, Pod, Zeroable, Copy, Clone)]
+pub struct BankConfig {
+    pub asset_weight_init: WrappedI80F48,
+    pub asset_weight_maint: WrappedI80F48,
+
+    pub liability_weight_init: WrappedI80F48,
+    pub liability_weight_maint: WrappedI80F48,
+
+    pub deposit_limit: u64,
+
+    pub interest_rate_config: InterestRateConfig,
+    pub operational_state: BankOperationalState,
+
+    pub oracle_setup: OracleSetup,
+    pub oracle_keys: [Pubkey; MAX_ORACLE_KEYS],
+
+    // Note: Pubkey is aligned 1, so borrow_limit is the first aligned-8 value after deposit_limit
+    pub _pad0: [u8; 6], // Bank state (1) + Oracle Setup (1) + 6 = 8
+
+    pub borrow_limit: u64,
+
+    pub risk_tier: RiskTier,
+
+    /// Determines what kinds of assets users of this bank can interact with.
+    /// Options:
+    /// * ASSET_TAG_DEFAULT (0) - A regular asset that can be comingled with any other regular asset
+    ///   or with `ASSET_TAG_SOL`
+    /// * ASSET_TAG_SOL (1) - Accounts with a SOL position can comingle with **either**
+    /// `ASSET_TAG_DEFAULT` or `ASSET_TAG_STAKED` positions, but not both
+    /// * ASSET_TAG_STAKED (2) - Staked SOL assets. Accounts with a STAKED position can only deposit
+    /// other STAKED assets or SOL (`ASSET_TAG_SOL`) and can only borrow SOL
+    pub asset_tag: u8,
+
+    pub _pad1: [u8; 6],
+
+    /// USD denominated limit for calculating asset value for initialization margin requirements.
+    /// Example, if total SOL deposits are equal to $1M and the limit it set to $500K,
+    /// then SOL assets will be discounted by 50%.
+    ///
+    /// In other words the max value of liabilities that can be backed by the asset is $500K.
+    /// This is useful for limiting the damage of orcale attacks.
+    ///
+    /// Value is UI USD value, for example value 100 -> $100
+    pub total_asset_value_init_limit: u64,
+
+    /// Time window in seconds for the oracle price feed to be considered live.
+    pub oracle_max_age: u16,
+
+    // Note: 6 bytes of padding to next 8 byte alignment, then end padding
+    pub _padding0: [u8; 6],
+    pub _padding1: [u8; 32],
+}
+
+#[repr(C)]
+#[derive(Debug, PartialEq, Copy, Clone, Pod, Zeroable)]
+pub struct InterestRateConfig {
+    // Curve Params
+    pub optimal_utilization_rate: WrappedI80F48,
+    pub plateau_interest_rate: WrappedI80F48,
+    pub max_interest_rate: WrappedI80F48,
+
+    // Fees
+    /// Goes to insurance, funds `collected_insurance_fees_outstanding`
+    pub insurance_fee_fixed_apr: WrappedI80F48,
+    /// Goes to insurance, funds `collected_insurance_fees_outstanding`
+    pub insurance_ir_fee: WrappedI80F48,
+    /// Earned by the group, goes to `collected_group_fees_outstanding`
+    pub protocol_fixed_fee_apr: WrappedI80F48,
+    /// Earned by the group, goes to `collected_group_fees_outstanding`
+    pub protocol_ir_fee: WrappedI80F48,
+    pub protocol_origination_fee: WrappedI80F48,
+
+    pub _padding0: [u8; 16],
+    pub _padding1: [[u8; 32]; 3],
+}
+
+#[repr(u8)]
+#[derive(Debug, PartialEq, Copy, Clone, Default)]
+pub enum RiskTier {
+    #[default]
+    Collateral = 0,
+    /// ## Isolated Risk
+    /// Assets in this trance can be borrowed only in isolation.
+    /// They can't be borrowed together with other assets.
+    ///
+    /// For example, if users has USDC, and wants to borrow XYZ which is isolated,
+    /// they can't borrow XYZ together with SOL, only XYZ alone.
+    Isolated = 1,
+}
+unsafe impl Zeroable for RiskTier {}
+unsafe impl Pod for RiskTier {}
+
+#[repr(u8)]
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum BankOperationalState {
+    Paused,
+    Operational,
+    ReduceOnly,
+}
+unsafe impl Zeroable for BankOperationalState {}
+unsafe impl Pod for BankOperationalState {}
+
+#[repr(u8)]
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum OracleSetup {
+    None,
+    PythLegacy,
+    SwitchboardV2,
+    PythPushOracle,
+    SwitchboardPull,
+    StakedWithPythPush,
+}
+unsafe impl Zeroable for OracleSetup {}
+unsafe impl Pod for OracleSetup {}
