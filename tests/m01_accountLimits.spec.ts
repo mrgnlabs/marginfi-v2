dcae3e6673a4fb52620853b1540962bb85874fb2
jgur-psyops <jonathan@mrgn.group>
2025-05-01 04:41:11 -0400

    Validate that liquidate BARELY avoids going OOM (and ONLY if the bump allocator releases the emode reconcilliation)

--- tests/m01_accountLimits.spec.ts
+++ tests/m01_accountLimits.spec.ts
@@ -325,86 +325,106 @@ describe("Pyth pull oracles in localnet", () => {
     console.log("No memory failures detected on " + MAX_BALANCES + " accounts");
   });
 
-  // it("(admin) vastly increase last bank liability ratio to make user 0 unhealthy", async () => {
-  //   let config = defaultBankConfigOptRaw();
-  //   config.liabilityWeightInit = bigNumberToWrappedI80F48(210); // 21000%
-  //   config.liabilityWeightMaint = bigNumberToWrappedI80F48(200); // 20000%
+  it("(admin) vastly increase last bank liability ratio to make user 0 unhealthy", async () => {
+    let config = defaultBankConfigOptRaw();
+    config.liabilityWeightInit = bigNumberToWrappedI80F48(210); // 21000%
+    config.liabilityWeightMaint = bigNumberToWrappedI80F48(200); // 20000%
 
-  //   let tx = new Transaction().add(
-  //     await configureBank(groupAdmin.mrgnBankrunProgram, {
-  //       bank: banks[MAX_BALANCES - 1],
-  //       bankConfigOpt: config,
-  //     })
-  //   );
-  //   tx.recentBlockhash = await getBankrunBlockhash(bankrunContext);
-  //   tx.sign(groupAdmin.wallet);
-  //   await banksClient.processTransaction(tx);
-  // });
+    let tx = new Transaction().add(
+      await configureBank(groupAdmin.mrgnBankrunProgram, {
+        bank: banks[MAX_BALANCES - 1],
+        bankConfigOpt: config,
+      })
+    );
+    tx.recentBlockhash = await getBankrunBlockhash(bankrunContext);
+    tx.sign(groupAdmin.wallet);
+    await banksClient.processTransaction(tx);
+  });
 
-  // it("(user 1) Liquidates user 0", async () => {
-  //   const liquidatee = users[0];
-  //   const liquidateeAccount = liquidatee.accounts.get(USER_ACCOUNT_THROWAWAY);
-  //   const liquidator = users[1];
-  //   const liquidatorAccount = liquidator.accounts.get(USER_ACCOUNT_THROWAWAY);
-  //   const depositAmount = new BN(100 * 10 ** ecosystem.lstAlphaDecimals);
-  //   const liquidateAmount = new BN(0.01 * 10 ** ecosystem.lstAlphaDecimals);
+  it("(user 1) Liquidates user 0", async () => {
+    const liquidatee = users[0];
+    const liquidateeAccount = liquidatee.accounts.get(USER_ACCOUNT_THROWAWAY);
+    const liquidator = users[1];
+    const liquidatorAccount = liquidator.accounts.get(USER_ACCOUNT_THROWAWAY);
+    const depositAmount = new BN(100 * 10 ** ecosystem.lstAlphaDecimals);
+    const liquidateAmount = new BN(0.01 * 10 ** ecosystem.lstAlphaDecimals);
 
-  //   const remainingAccounts: PublicKey[][] = [];
-  //   for (let i=0; i<MAX_BALANCES; i++){
-  //     remainingAccounts.push([banks[i], oracles.pythPullLst.publicKey]);
-  //     console.log("bank: " + banks[i]);
-  //   }
+    const remainingAccounts: PublicKey[][] = [];
+    for (let i = 0; i < MAX_BALANCES; i++) {
+      remainingAccounts.push([banks[i], oracles.pythPullLst.publicKey]);
+      console.log("bank: " + banks[i]);
+    }
 
-  //   // Deposit some funds to operate as a liquidator...
-  //   let tx = new Transaction();
-  //   tx.add(
-  //     await depositIx(liquidator.mrgnBankrunProgram, {
-  //       marginfiAccount: liquidatorAccount,
-  //       bank: banks[0],
-  //       tokenAccount: liquidator.lstAlphaAccount,
-  //       amount: depositAmount,
-  //       depositUpToLimit: false,
-  //     })
-  //   );
-  //   tx.recentBlockhash = await getBankrunBlockhash(bankrunContext);
-  //   tx.sign(liquidator.wallet);
-  //   await banksClient.tryProcessTransaction(tx);
+    // Deposit some funds to operate as a liquidator...
+    let tx = new Transaction();
+    tx.add(
+      await depositIx(liquidator.mrgnBankrunProgram, {
+        marginfiAccount: liquidatorAccount,
+        bank: banks[0],
+        tokenAccount: liquidator.lstAlphaAccount,
+        amount: depositAmount,
+        depositUpToLimit: false,
+      })
+    );
+    tx.recentBlockhash = await getBankrunBlockhash(bankrunContext);
+    tx.sign(liquidator.wallet);
+    await banksClient.tryProcessTransaction(tx);
 
-  //   const liquidateeAcc = await bankrunProgram.account.marginfiAccount.fetch(liquidateeAccount);
-  //   dumpAccBalances(liquidateeAcc);
-  //   const liquidatorAcc = await bankrunProgram.account.marginfiAccount.fetch(liquidatorAccount);
-  //   dumpAccBalances(liquidatorAcc);
+    const liquidateeAcc = await bankrunProgram.account.marginfiAccount.fetch(
+      liquidateeAccount
+    );
+    dumpAccBalances(liquidateeAcc);
+    const liquidatorAcc = await bankrunProgram.account.marginfiAccount.fetch(
+      liquidatorAccount
+    );
+    dumpAccBalances(liquidatorAcc);
 
-  //   tx = new Transaction().add(
-  //     ComputeBudgetProgram.setComputeUnitLimit({ units: 400_000 }),
-  //     await liquidateIx(liquidator.mrgnBankrunProgram, {
-  //       assetBankKey: banks[0],
-  //       liabilityBankKey: banks[MAX_BALANCES - 1],
-  //       liquidatorMarginfiAccount: liquidatorAccount,
-  //       liquidateeMarginfiAccount: liquidateeAccount,
-  //       remaining: [
-  //         oracles.pythPullLst.publicKey, // asset oracle
-  //         oracles.pythPullLst.publicKey, // liab oracle
+    tx = new Transaction().add(
+      ComputeBudgetProgram.setComputeUnitLimit({ units: 2_000_000 }),
+      await liquidateIx(liquidator.mrgnBankrunProgram, {
+        assetBankKey: banks[0],
+        liabilityBankKey: banks[MAX_BALANCES - 1],
+        liquidatorMarginfiAccount: liquidatorAccount,
+        liquidateeMarginfiAccount: liquidateeAccount,
+        remaining: [
+          oracles.pythPullLst.publicKey, // asset oracle
+          oracles.pythPullLst.publicKey, // liab oracle
 
-  //         ...composeRemainingAccounts([
-  //           // liquidator accounts
-  //           [banks[0], oracles.pythPullLst.publicKey],
-  //           [banks[MAX_BALANCES - 1], oracles.pythPullLst.publicKey],
-  //         ]),
+          ...composeRemainingAccounts([
+            // liquidator accounts
+            [banks[0], oracles.pythPullLst.publicKey],
+            [banks[MAX_BALANCES - 1], oracles.pythPullLst.publicKey],
+          ]),
 
-  //         ...composeRemainingAccounts(
-  //           // liquidatee accounts
-  //           remainingAccounts,
-  //         ),
-  //       ],
-  //       amount: liquidateAmount,
-  //     })
-  //   );
-  //   tx.recentBlockhash = await getBankrunBlockhash(bankrunContext);
-  //   tx.sign(liquidator.wallet);
-  //   let result = await banksClient.tryProcessTransaction(tx);
-  //   dumpBankrunLogs(result);
-  // });
+          ...composeRemainingAccounts(
+            // liquidatee accounts
+            remainingAccounts
+          ),
+        ],
+        amount: liquidateAmount,
+      })
+    );
+    tx.recentBlockhash = await getBankrunBlockhash(bankrunContext);
+    tx.sign(liquidator.wallet);
+    let result = await banksClient.tryProcessTransaction(tx);
+    dumpBankrunLogs(result);
+
+    // Throws if the error is not OOM.
+    if (result.result) {
+      const logs = result.meta.logMessages;
+      const isOOM = logs.some((msg) =>
+        msg.toLowerCase().includes("memory allocation failed, out of memory")
+      );
+
+      if (isOOM) {
+        console.warn(`⚠️ \t OOM during liquidate: \n`, logs);
+        assert.ok(false);
+      } else {
+        // anything other than OOM should blow up the test
+        throw new Error(`Unexpected liquidate failure}: ` + logs.join("\n"));
+      }
+    }
+  });
 
   async function addBankTest(options: {
     assetTag?: number;
