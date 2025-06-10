fd46dc7f15b180cb8719e37afe747874b8bfb682
jgur-psyops <jonathan@mrgn.group>
2025-06-10 16:44:47 -0400

    Fix compilation errors

--- programs/marginfi/src/constants.rs
+++ programs/marginfi/src/constants.rs
@@ -181,3 +181,11 @@ pub const ASSET_TAG_SOL: u8 = 1;
 /// Staked SOL assets. Accounts with a STAKED position can only deposit other STAKED assets or SOL
 /// (`ASSET_TAG_SOL`) and can only borrow SOL (`ASSET_TAG_SOL`)
 pub const ASSET_TAG_STAKED: u8 = 2;
+
+// TODO move this to the global fee wallet eventually
+/// A nominal fee paid to the global wallet when intiating an account transfer. Primarily intended
+/// to avoid spammming account migration, which is mildly annoying to backend systems that track the
+/// state of accounts.
+/// * Should be ~ $0.01 or around that magnitude
+/// * In lamports
+pub const ACCOUNT_TRANSFER_FEE: u64 = 100_000;
--- programs/marginfi/src/instructions/marginfi_account/transfer_account.rs
+++ programs/marginfi/src/instructions/marginfi_account/transfer_account.rs
@@ -1,31 +1,26 @@
 use crate::{
+    constants::ACCOUNT_TRANSFER_FEE,
     events::{AccountEventHeader, MarginfiAccountTransferAccountAuthorityEvent},
     prelude::*,
-    state::marginfi_account::{LendingAccount, MarginfiAccount, ACCOUNT_DISABLED, ACCOUNT_TRANSFER_AUTHORITY_ALLOWED},
+    state::marginfi_account::{LendingAccount, MarginfiAccount, ACCOUNT_DISABLED},
 };
 use anchor_lang::prelude::*;
+use bytemuck::Zeroable;
 
-pub fn transfer_account_authority(
-    ctx: Context<TransferAccountAuthority>,
-) -> MarginfiResult {
-    anchor_lang::system_program::transfer(ctx.accounts.transfer_fee(), 1000)?;
+pub fn transfer_account_authority(ctx: Context<TransferAccountAuthority>) -> MarginfiResult {
+    // The global fee wallet claims a (nominal) fee
+    anchor_lang::system_program::transfer(ctx.accounts.transfer_fee(), ACCOUNT_TRANSFER_FEE)?;
 
     let mut old_account = ctx.accounts.old_marginfi_account.load_mut()?;
-    check!(
-        !old_account.get_flag(ACCOUNT_DISABLED)
-            && old_account.get_flag(ACCOUNT_TRANSFER_AUTHORITY_ALLOWED),
-        MarginfiError::IllegalAccountAuthorityTransfer
-    );
 
     let mut new_account = ctx.accounts.new_marginfi_account.load_init()?;
     new_account.initialize(old_account.group, ctx.accounts.new_authority.key());
     new_account.lending_account = old_account.lending_account;
     new_account.emissions_destination_account = old_account.emissions_destination_account;
-    new_account.account_flags = old_account.account_flags & !ACCOUNT_TRANSFER_AUTHORITY_ALLOWED;
+    new_account.account_flags = old_account.account_flags;
     new_account.migrated_from = ctx.accounts.old_marginfi_account.key();
 
     old_account.lending_account = LendingAccount::zeroed();
-    old_account.unset_flag(ACCOUNT_TRANSFER_AUTHORITY_ALLOWED);
     old_account.set_flag(ACCOUNT_DISABLED);
 
     emit!(MarginfiAccountTransferAccountAuthorityEvent {
@@ -44,29 +39,31 @@ pub fn transfer_account_authority(
 
 #[derive(Accounts)]
 pub struct TransferAccountAuthority<'info> {
-    #[account(mut, has_one = authority, has_one = group)]
+    #[account(
+        mut, 
+        has_one = authority, 
+        has_one = group
+    )]
     pub old_marginfi_account: AccountLoader<'info, MarginfiAccount>,
 
     #[account(
         init,
-        payer = fee_payer,
+        payer = authority,
         space = 8 + std::mem::size_of::<MarginfiAccount>()
     )]
     pub new_marginfi_account: AccountLoader<'info, MarginfiAccount>,
 
     pub group: AccountLoader<'info, MarginfiGroup>,
 
+    #[account(mut)]
     pub authority: Signer<'info>,
 
-    /// CHECK: New authority is unchecked
+    /// CHECK: WARN: New authority is completely unchecked
     pub new_authority: AccountInfo<'info>,
 
     /// CHECK: Validated against group fee state cache
     #[account(mut)]
     pub global_fee_wallet: AccountInfo<'info>,
-
-    #[account(mut)]
-    pub fee_payer: Signer<'info>,
 
     pub system_program: Program<'info, System>,
 }
@@ -78,7 +75,7 @@ impl<'info> TransferAccountAuthority<'info> {
         CpiContext::new(
             self.system_program.to_account_info(),
             anchor_lang::system_program::Transfer {
-                from: self.fee_payer.to_account_info(),
+                from: self.authority.to_account_info(),
                 to: self.global_fee_wallet.to_account_info(),
             },
         )
