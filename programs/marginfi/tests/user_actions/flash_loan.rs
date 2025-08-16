db8cc7044aee4e0a7c04a806153b564bc6c3a6bc
Henry Elder <12613144+Henry-E@users.noreply.github.com>
2025-08-16 11:17:56 +0000

    restore original test

--- programs/marginfi/tests/user_actions/flash_loan.rs
+++ programs/marginfi/tests/user_actions/flash_loan.rs
@@ -13,13 +13,14 @@ use solana_sdk::{
 // Flashloan tests
 // 1. Flashloan success (1 action)
 // 2. Flashloan success (3 actions)
-// 3. Flashloan fails because of bad account health
-// 4. Flashloan fails because of non whitelisted account
-// 5. Flashloan fails because of missing `end_flashloan` ix
-// 6. Flashloan fails because of invalid instructions sysvar
-// 7. Flashloan fails because of invalid `end_flashloan` ix order
-// 8. Flashloan fails because `end_flashloan` ix is for another account
-// 9. Flashloan fails because account is already in a flashloan
+// 3. Flashloan fails because account transfer during flashloan
+// 4. Flashloan fails because of bad account health
+// 5. Flashloan fails because of non whitelisted account
+// 6. Flashloan fails because of missing `end_flashloan` ix
+// 7. Flashloan fails because of invalid instructions sysvar
+// 8. Flashloan fails because of invalid `end_flashloan` ix order
+// 9. Flashloan fails because `end_flashloan` ix is for another account
+// 10. Flashloan fails because account is already in a flashloan
 
 #[tokio::test]
 async fn flashloan_success_1op() -> anyhow::Result<()> {
@@ -119,7 +120,7 @@ async fn flashloan_success_3op() -> anyhow::Result<()> {
 }
 
 #[tokio::test]
-async fn flashloan_fail_account_health() -> anyhow::Result<()> {
+async fn flashloan_fail_account_transfer_during_flashloan() -> anyhow::Result<()> {
     let test_f = TestFixture::new(Some(TestSettings::all_banks_payer_not_admin())).await;
 
     let sol_bank = test_f.get_bank(&BankMint::Sol);
@@ -177,7 +178,44 @@ async fn flashloan_fail_account_health() -> anyhow::Result<()> {
         flash_loan_result.unwrap_err(),
         MarginfiError::AccountInFlashloan
     );
-    //assert!(flash_loan_result.is_ok());
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn flashloan_fail_account_health() -> anyhow::Result<()> {
+    let test_f = TestFixture::new(Some(TestSettings::all_banks_payer_not_admin())).await;
+
+    let sol_bank = test_f.get_bank(&BankMint::Sol);
+
+    // Fund SOL lender
+    let lender_mfi_account_f = test_f.create_marginfi_account().await;
+    let lender_token_account_f_sol = test_f
+        .sol_mint
+        .create_token_account_and_mint_to(1_000)
+        .await;
+    lender_mfi_account_f
+        .try_bank_deposit(lender_token_account_f_sol.key, sol_bank, 1_000, None)
+        .await?;
+
+    // Fund SOL borrower
+    let borrower_mfi_account_f = test_f.create_marginfi_account().await;
+
+    let borrower_token_account_f_sol = test_f.sol_mint.create_empty_token_account().await;
+
+    // Borrow SOL
+    let borrow_ix = borrower_mfi_account_f
+        .make_bank_borrow_ix(borrower_token_account_f_sol.key, sol_bank, 1_000)
+        .await;
+
+    let flash_loan_result = borrower_mfi_account_f
+        .try_flashloan(vec![borrow_ix], vec![], vec![sol_bank.key], None)
+        .await;
+
+    assert_custom_error!(
+        flash_loan_result.unwrap_err(),
+        MarginfiError::RiskEngineInitRejected
+    );
 
     Ok(())
 }
