d2ccd9fca5c2cfcf33afce4363a0985286cda3ed
jgur-psyops <jonathan@mrgn.group>
2025-09-09 00:38:27 -0400

    Fix lint 2

--- test-utils/src/test.rs
+++ test-utils/src/test.rs
@@ -1,7 +1,7 @@
 use super::marginfi_account::MarginfiAccountFixture;
-use crate::{bank::BankFixture, marginfi_group::*, native, spl::*, utils::*};
 #[cfg(feature = "transfer-hook")]
 use crate::transfer_hook::TEST_HOOK_ID;
+use crate::{bank::BankFixture, marginfi_group::*, native, spl::*, utils::*};
 
 use anchor_lang::prelude::*;
 use bincode::deserialize;
