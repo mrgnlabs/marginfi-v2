600e006378ece21406429396d3ce499f801f7cdb
Henry Elder <12613144+Henry-E@users.noreply.github.com>
2025-10-02 16:46:12 +0000

    feat: separate fee payer and signer in transfer_account
    Split authority into two roles:
    - authority: signs to authorize transfer
    - fee_payer: pays fees and rent

--- tests/12_transfer_account.spec.ts
+++ tests/12_transfer_account.spec.ts
@@ -1,4 +1,4 @@
-import { Keypair, Transaction } from "@solana/web3.js";
+import { Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
 import { Program, workspace } from "@coral-xyz/anchor";
 import { Marginfi } from "../target/types/marginfi";
 import { marginfiGroup, users, globalFeeWallet } from "./rootHooks";
@@ -164,4 +164,154 @@ describe("Transfer account authority", () => {
   });
 
   // TODO emissions destination and/or flags?
+
+  it("(user 0) transfer account with separate fee payer - happy path", async () => {
+    const oldAccKeypair = Keypair.generate();
+    const newAccKeypair = Keypair.generate();
+    const newAuthority = Keypair.generate();
+    const separateFeePayer = Keypair.generate();
+
+    // Fund the separate fee payer
+    let fundTx = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: users[0].wallet.publicKey,
+        toPubkey: separateFeePayer.publicKey,
+        lamports: 1 * LAMPORTS_PER_SOL,
+      })
+    );
+    await users[0].mrgnProgram.provider.sendAndConfirm(fundTx, []);
+
+    // Create the old account
+    let tx = new Transaction().add(
+      await accountInit(users[0].mrgnProgram, {
+        marginfiGroup: marginfiGroup.publicKey,
+        marginfiAccount: oldAccKeypair.publicKey,
+        authority: users[0].wallet.publicKey,
+        feePayer: users[0].wallet.publicKey,
+      })
+    );
+    await users[0].mrgnProgram.provider.sendAndConfirm(tx, [oldAccKeypair]);
+
+    // Transfer with separate fee payer
+    let tx2 = new Transaction().add(
+      await transferAccountAuthorityIx(users[0].mrgnProgram, {
+        oldAccount: oldAccKeypair.publicKey,
+        newAccount: newAccKeypair.publicKey,
+        newAuthority: newAuthority.publicKey,
+        globalFeeWallet: globalFeeWallet,
+        feePayer: separateFeePayer.publicKey,
+      })
+    );
+
+    await users[0].mrgnProgram.provider.sendAndConfirm(tx2, [
+      newAccKeypair,
+      separateFeePayer,
+    ]);
+
+    const newAcc = await program.account.marginfiAccount.fetch(
+      newAccKeypair.publicKey
+    );
+    const oldAcc = await program.account.marginfiAccount.fetch(
+      oldAccKeypair.publicKey
+    );
+
+    assertKeysEqual(newAcc.authority, newAuthority.publicKey);
+    assertKeysEqual(newAcc.migratedFrom, oldAccKeypair.publicKey);
+    assertKeyDefault(newAcc.migratedTo);
+    assertBNEqual(oldAcc.accountFlags, ACCOUNT_DISABLED);
+    assertKeysEqual(oldAcc.migratedTo, newAccKeypair.publicKey);
+  });
+
+  it("(user 0) transfer account after authority wallet ownership transferred to Token Program", async () => {
+    const { TOKEN_PROGRAM_ID } = await import("@solana/spl-token");
+    const authorityKeypair = Keypair.generate();
+    const marginfiAccountKeypair = Keypair.generate();
+
+    // Fund the authority wallet
+    let fundTx = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: users[0].wallet.publicKey,
+        toPubkey: authorityKeypair.publicKey,
+        lamports: 10 * LAMPORTS_PER_SOL,
+      })
+    );
+    await users[0].mrgnProgram.provider.sendAndConfirm(fundTx, []);
+
+    // Create marginfi account with this authority
+    let createTx = new Transaction().add(
+      await accountInit(users[0].mrgnProgram, {
+        marginfiGroup: marginfiGroup.publicKey,
+        marginfiAccount: marginfiAccountKeypair.publicKey,
+        authority: authorityKeypair.publicKey,
+        feePayer: authorityKeypair.publicKey,
+      })
+    );
+    await users[0].mrgnProgram.provider.sendAndConfirm(createTx, [
+      authorityKeypair,
+      marginfiAccountKeypair,
+    ]);
+
+    // Transfer ownership of the authority wallet to Token Program (arbitrary program)
+    let assignTx = new Transaction().add(
+      SystemProgram.assign({
+        accountPubkey: authorityKeypair.publicKey,
+        programId: TOKEN_PROGRAM_ID, // Use Token Program instead of marginfi
+      })
+    );
+    await users[0].mrgnProgram.provider.sendAndConfirm(assignTx, [
+      authorityKeypair,
+    ]);
+
+    // Verify the authority wallet is now owned by Token Program
+    const authorityAccountInfo = await program.provider.connection.getAccountInfo(
+      authorityKeypair.publicKey
+    );
+    assertKeysEqual(authorityAccountInfo.owner, TOKEN_PROGRAM_ID);
+
+    // Now transfer the marginfi account using the authority whose wallet ownership was transferred to Token Program
+    // The signature should still work because we're signing with the private key
+    const newAccKeypair = Keypair.generate();
+    const newAuthority = Keypair.generate();
+
+    let transferIx = await transferAccountAuthorityIx(users[0].mrgnProgram, {
+      oldAccount: marginfiAccountKeypair.publicKey,
+      newAccount: newAccKeypair.publicKey,
+      newAuthority: newAuthority.publicKey,
+      globalFeeWallet: globalFeeWallet,
+      authority: authorityKeypair.publicKey,
+      feePayer: users[0].wallet.publicKey,
+    });
+
+    // Build transaction with users[0] as fee payer (since authorityKeypair can no longer pay fees after ownership transfer)
+    // But authorityKeypair must still sign as the authority
+    let transferTx = new Transaction().add(transferIx);
+    transferTx.feePayer = users[0].wallet.publicKey;
+    const { blockhash, lastValidBlockHeight } =
+      await program.provider.connection.getLatestBlockhash();
+    transferTx.recentBlockhash = blockhash;
+
+    // Sign with both the authority (whose wallet is now owned by Token Program) and the new account keypair
+    // The key insight: signature verification works regardless of which program owns the authority wallet
+    transferTx.partialSign(authorityKeypair, newAccKeypair);
+
+    // Also need the fee payer to sign
+    await users[0].mrgnProgram.provider.wallet.signTransaction(transferTx);
+
+    // This should succeed - signature verification works regardless of wallet ownership
+    const signature = await program.provider.connection.sendRawTransaction(
+      transferTx.serialize()
+    );
+    await program.provider.connection.confirmTransaction({
+      signature,
+      blockhash,
+      lastValidBlockHeight,
+    });
+
+    // Verify the transfer succeeded
+    const newAcc = await program.account.marginfiAccount.fetch(
+      newAccKeypair.publicKey
+    );
+    assertKeysEqual(newAcc.authority, newAuthority.publicKey);
+    assertKeysEqual(newAcc.migratedFrom, marginfiAccountKeypair.publicKey);
+  });
 });
