4a3f0183d84e7b23acb41c427119a018720ac615
jgur-psyops <jonathan@mrgn.group>
2025-10-15 13:59:52 -0400

    Restructure config compact to use new points model

--- type-crate/src/types/interest_rate.rs
+++ type-crate/src/types/interest_rate.rs
@@ -4,6 +4,7 @@ use crate::assert_struct_size;
 use anchor_lang::prelude::*;
 
 use bytemuck::{Pod, Zeroable};
+use fixed::types::I80F48;
 
 use super::WrappedI80F48;
 
@@ -47,12 +48,12 @@ pub struct InterestRateConfig {
     /// 0 = legacy three point curve, 1 = multi-point curve.
     pub curve_type: u8,
 
-    pub _padding1: [[u8; 32]; 1],
-    pub _padding2: [[u8; 16]; 1],
-    pub _padding3: [[u8; 8]; 1],
-    pub _padding4: [[u8; 4]; 1],
-    pub _padding5: [[u8; 2]; 1],
-    pub _padding6: [[u8; 1]; 1],
+    // Pad to nearest 8-byte multiple
+    pub _pad0: [u8; 7],
+
+    pub _padding1: [u8; 32],
+    pub _padding2: [u8; 16],
+    pub _padding3: [u8; 8],
 }
 
 #[cfg_attr(feature = "anchor", derive(AnchorDeserialize, AnchorSerialize))]
@@ -81,6 +82,7 @@ impl RatePoint {
     }
 }
 
+// TODO deprecate
 #[cfg_attr(feature = "anchor", derive(AnchorDeserialize, AnchorSerialize))]
 #[derive(Default, Clone, Debug, PartialEq, Eq)]
 pub struct InterestRateConfigOpt {
@@ -96,33 +98,18 @@ pub struct InterestRateConfigOpt {
     pub protocol_fixed_fee_apr: Option<WrappedI80F48>,
     pub protocol_ir_fee: Option<WrappedI80F48>,
     pub protocol_origination_fee: Option<WrappedI80F48>,
-    /// The base rate at utilizatation = 0
-    /// * a %, as u32, out of 1000%, e.g. 100% = 0.1 * u32::MAX
-    pub zero_util_rate: Option<u32>,
-    /// The base rate at utilizatation = 100
-    /// * a %, as u32, out of 1000%, e.g. 100% = 0.1 * u32::MAX
-    pub hundred_util_rate: Option<u32>,
-    /// The base rate at various points between 0 and 100%, exclusive. Essentially a piece-wise
-    /// linear curve.
-    /// * always in ascending order, e.g. points[0] = first kink point, points[1] = second kink
-    ///   point, and so forth.
-    /// * points where util = 0 are unused
-    pub points: Option<[RatePoint; 5]>,
-    /// Determines which interest rate curve implementation is active.
-    /// 0 = legacy three point curve, 1 = multi-point curve.
-    pub curve_type: Option<u8>,
 }
 
 #[repr(C)]
 #[cfg_attr(feature = "anchor", derive(AnchorDeserialize, AnchorSerialize))]
 #[derive(Default, Debug, PartialEq, Eq)]
 pub struct InterestRateConfigCompact {
-    // TODO deprecate
-    pub optimal_utilization_rate: WrappedI80F48,
-    // TODO deprecate
-    pub plateau_interest_rate: WrappedI80F48,
-    // TODO deprecate
-    pub max_interest_rate: WrappedI80F48,
+    // // TODO deprecate
+    // pub optimal_utilization_rate: WrappedI80F48,
+    // // TODO deprecate
+    // pub plateau_interest_rate: WrappedI80F48,
+    // // TODO deprecate
+    // pub max_interest_rate: WrappedI80F48,
 
     // Fees
     pub insurance_fee_fixed_apr: WrappedI80F48,
@@ -143,18 +130,21 @@ pub struct InterestRateConfigCompact {
     ///   point, and so forth.
     /// * points where util = 0 are unused
     pub points: [RatePoint; 5],
+
     /// Determines which interest rate curve implementation is active.
     /// 0 = legacy three point curve, 1 = multi-point curve.
     pub curve_type: u8,
-    pub _padding0: [u8; 7],
+
+    // Pad to nearest 8-byte multiple
+    pub _pad0: [u8; 7],
 }
 
 impl From<InterestRateConfigCompact> for InterestRateConfig {
     fn from(ir_config: InterestRateConfigCompact) -> Self {
         InterestRateConfig {
-            optimal_utilization_rate: ir_config.optimal_utilization_rate,
-            plateau_interest_rate: ir_config.plateau_interest_rate,
-            max_interest_rate: ir_config.max_interest_rate,
+            optimal_utilization_rate: I80F48::ZERO.into(),
+            plateau_interest_rate: I80F48::ZERO.into(),
+            max_interest_rate: I80F48::ZERO.into(),
             insurance_fee_fixed_apr: ir_config.insurance_fee_fixed_apr,
             insurance_ir_fee: ir_config.insurance_ir_fee,
             protocol_fixed_fee_apr: ir_config.protocol_fixed_fee_apr,
@@ -164,12 +154,10 @@ impl From<InterestRateConfigCompact> for InterestRateConfig {
             hundred_util_rate: ir_config.hundred_util_rate,
             points: ir_config.points,
             curve_type: ir_config.curve_type,
-            _padding1: [[0; 32]; 1],
-            _padding2: [[0; 16]; 1],
-            _padding3: [[0; 8]; 1],
-            _padding4: [[0; 4]; 1],
-            _padding5: [[0; 2]; 1],
-            _padding6: [[0; 1]; 1],
+            _pad0: [0; 7],
+            _padding1: [0; 32],
+            _padding2: [0; 16],
+            _padding3: [0; 8],
         }
     }
 }
@@ -177,9 +165,6 @@ impl From<InterestRateConfigCompact> for InterestRateConfig {
 impl From<InterestRateConfig> for InterestRateConfigCompact {
     fn from(ir_config: InterestRateConfig) -> Self {
         InterestRateConfigCompact {
-            optimal_utilization_rate: ir_config.optimal_utilization_rate,
-            plateau_interest_rate: ir_config.plateau_interest_rate,
-            max_interest_rate: ir_config.max_interest_rate,
             insurance_fee_fixed_apr: ir_config.insurance_fee_fixed_apr,
             insurance_ir_fee: ir_config.insurance_ir_fee,
             protocol_fixed_fee_apr: ir_config.protocol_fixed_fee_apr,
@@ -189,7 +174,7 @@ impl From<InterestRateConfig> for InterestRateConfigCompact {
             hundred_util_rate: ir_config.hundred_util_rate,
             points: ir_config.points,
             curve_type: ir_config.curve_type,
-            _padding0: [0; 7],
+            _pad0: [0; 7],
         }
     }
 }
